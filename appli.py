3f1a41d9e9dc9889c88c312502d05c6a4548662a
JAG <JAG@macbook-pro-de-jag-3.home>
2018-05-25 23:14:44 +0200

    update

--- appli.py
+++ appli.py
@@ -4,21 +4,23 @@ from flask import render_template
 import pandas as pd
 import nltk
 from fonction_ import *
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import NMF
 from nltk import word_tokenize
+
 def tokenize_tags(x):
     wt = word_tokenize(x)
     return wt
 
-df1 = pd.read_csv('tags_and_titles_.csv', sep='\t', encoding='utf-8')
-df1['tags_F'] = df1['tags_1'].map(tokenize_tags)
-df2 = pd.read_csv("features.csv", sep='\t', encoding='utf-8')
-SVC_ = joblib.load('svc_1.pkl')
+SVC_ = joblib.load('svc_3.pkl')
+TfidfVec = joblib.load('vectorizer3.pkl')
+features = np.array(TfidfVec.get_feature_names())  #feature_array
+print('features shape',features.shape)
+all_tags = joblib.load('tags_output3.pkl')
+NMF_ = joblib.load('nmf.pkl')
 
 app = Flask(__name__)
 
-#df1 = pd.read_csv('tags_and_titles.csv', sep='\t', encoding='utf-8')
-#print('file 1 has been read', df1.shape)
 
 @app.route('/')
 def home():
@@ -31,32 +33,7 @@ def text_box():
     question = request.form['text']
     print("question is" , question)
 
-
-
-    #print('file 1 has been read', df1.shape)
-    #print('file 1 columns', df1.columns)
-    #toto = df1['tags_F'].iloc[0]
-    #print(toto)
-    #print(type(toto))
-
-
-
-    tag_set_F = set()
-    total_sample = 30000
-    for i in range(0, total_sample):
-        tag = df1['tags_F'].iloc[i]
-        for j in range(0, len(tag)):
-            tag_set_F.add(tag[j])
-    all_tags = list(tag_set_F)
-    print('all_tags[0]',all_tags[0] )
-    titles = list(df1['title_5'])
-
-
-
-    features = list(df2['features'])
-    print('file 2 has been read', df2.shape)
-
-    all_data = recommander_(question,all_tags, features,titles, SVC_)
+    all_data = recommander_f(question,all_tags, TfidfVec, SVC_, NMF_)
     print(all_data)
 
     if (len(all_data) == 0):
--- fonction_.py
+++ fonction_.py
@@ -27,6 +27,66 @@ class Reco_movie(object):
         self.reco = ""
 
 
+def recommander_f(q, alltags, TfidfVec_, SVC_prob, NMF):
+
+    warnings.filterwarnings('ignore')
+    tag_nb = 10
+
+
+
+    question = q
+    all_tags = alltags
+    features = np.array(TfidfVec_.get_feature_names())
+    tok_question = word_tokenize(question.lower())
+    retained_words = ''
+    for w in tok_question:
+        if (w in features):
+            retained_words += w + ' '
+
+    print('words',retained_words)
+
+    ## partie supervisée
+
+    X_tfidf = TfidfVec_.transform([retained_words])
+
+    prob_svc = SVC_prob.predict_proba(X_tfidf)#[-1])
+    print('loading')
+    tags_output = np.asarray(all_tags)
+    print(tags_output[0:100])
+    ind_svc = np.argpartition(prob_svc[0], range(len(prob_svc[0])))[-tag_nb:]
+    print(ind_svc)
+    print('tags output',len(tags_output))
+    print("\nSVC proba", list(reversed(prob_svc[0][ind_svc])))
+    svc_word = tags_output[ind_svc]
+    supervised = list(reversed(svc_word))
+
+
+    ## partie non supervisée
+
+
+    print("unsupervised")
+    post_topic_KL = NMF.transform(X_tfidf)
+    print("ok transform")
+    topic_most_prKL = post_topic_KL[0].argsort()[-2:]
+    print('ok sort')
+    witKL = []
+    for k in topic_most_prKL:
+        print(k)
+        topic = NMF.components_[k]
+        witKL += [features[i] for i in topic.argsort()[:-tag_nb//2 - 1:-1]]
+
+    inter_set_KL = set(witKL)
+    unsupervised = list(inter_set_KL)
+    print(unsupervised )
+
+    output = supervised + unsupervised
+    print(output)
+
+    return (output)
+
+
+
+
 def recommander_(q, alltags, features_,titles_, SVC_prob):
 
     warnings.filterwarnings('ignore')
